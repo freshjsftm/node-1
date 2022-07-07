1c8d4627e2cc31025b1146575fc01d74259aa70b
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-07 16:38:09 +0300

    Show work Resolving

--- Component.js
+++ Component.js
@@ -1,6 +1,8 @@
+const {MyMath} = require('./MyMath');
 class Component {
   render() {
     console.log("render component");
   }
 }
+MyMath.sub(5,2);
 exports.Component = Component;
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 "use strict";
 const {MyMath} = require('./MyMath');
 const {Component} = require('./Component');
+require('test');
 
 const res1 = MyMath.sum(2, 5);
 console.log("res1 = ", res1);
 const component1 = new Component();
 component1.render();
+
+//require
+//Resolving->Loading->Wrapping->Evalution->Caching
+/*
+  1) Core modules
+  2) File 
+      2.1) *.js
+      2.2) *.json
+  3)Directory
+      3.1) package.json->"main"
+      3.2) index.js | index.json
+  4)node_modules
+  5)throw new Error()
+*/
