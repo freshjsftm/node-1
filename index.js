fd3bae839d3bbf54f56196941ea942825a8955cc
Olena Zdanovska <freshjs.ftm@gmail.com>
2022-07-07 16:04:14 +0300

    Example require

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+const {MyMath} = require('./MyMath');
+const {Component} = require('./Component');
+
+const res1 = MyMath.sum(2, 5);
+console.log("res1 = ", res1);
+const component1 = new Component();
+component1.render();
